8e17b8459ca23f6318a7531925848cc62bc40907
Valentin Oreiller <valentin.oreiller@gmail.com>
2025-08-07 17:53:43 +0200

    mmmh

--- histoseg/data/datasets.py
+++ histoseg/data/datasets.py
@@ -1,14 +1,14 @@
-import re
 import json
+import re
 import zipfile
 from pathlib import Path
 from typing import Callable, Optional, Tuple
+
+import numpy as np
+import pandas as pd
 import torch
 from PIL import Image
-from torch.utils.data import get_worker_info
-from torchvision import tv_tensors
 from torchvision.transforms.v2 import functional as F
-import pandas as pd
 
 
 class ADE20KDataset(torch.utils.data.Dataset):
@@ -18,10 +18,43 @@ class ADE20KDataset(torch.utils.data.Dataset):
         self.transforms = transforms
 
         # Load image and annotation file paths
-        self.image_files = sorted(self.image_dir.glob("*.jpg"))
-        self.annotation_files = sorted(self.annotation_dir.glob("*.png"))
-        self
+        image_files = sorted(self.image_dir.glob("*.jpg"))
+        image_ids = []
+        annotation_files = []
+        for f in image_files:
+            image_id = f.stem
+            annotation_file = self.annotation_dir / f"{image_id}.png"
+            if not annotation_file.exists():
+                raise FileNotFoundError(
+                    f"Annotation file {annotation_file} does not exist."
+                )
+            image_ids.append(image_id)
+            annotation_files.append(annotation_file)
 
-        if len(self.image_files) != len(self.annotation_files):
-            raise ValueError("Number of images and annotations do not match.")
+        self.dataset = pd.DataFrame(
+            {
+                "image_id": image_ids,
+                "image_path": [str(f) for f in image_files],
+                "annotation_path": [str(f) for f in annotation_files],
+            }
+        )
 
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        if isinstance(idx, slice):
+            idx = idx.start if idx.start is not None else 0
+
+        row = self.dataset.iloc[idx]
+        image = Image.open(row["image_path"]).convert("RGB")
+        annotation = Image.open(row["annotation_path"])
+        instance_seg = np.array(annotation)[:, :, 1]  # green channel encodes instances
+        class_id_map = np.array(annotation)[:, :, 0]  # red channel encodes semantic cat
+        class_labels = np.unique(class_id_map)
+
+        if self.transforms:
+            image = self.transforms(image=image)
+            annotation = self.transforms(annotation)
+
+        return F.to_tensor(image), F.to_tensor(annotation)
